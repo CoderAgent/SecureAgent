f434c4c556d020afc7044e1167dac16ffe5403c4
Mohammad Tawhid Mahmud <33561106+Tawhid-Mahmud@users.noreply.github.com>
2024-11-22 21:51:00 -0500

    Update app.ts
    test 2 for pull request review

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,7 @@ import { processPullRequest } from "./review-agent";
 import { applyReview } from "./reviews";
 
 // This creates a new instance of the Octokit App class.
-const reviewApp = new App({
-  appId: env.GITHUB_APP_ID,
-  privateKey: env.GITHUB_PRIVATE_KEY,
-  webhooks: {
-    secret: env.GITHUB_WEBHOOK_SECRET,
-  },
-});
+
 
 const getChangesPerFile = async (payload: WebhookEventMap["pull_request"]) => {
   
